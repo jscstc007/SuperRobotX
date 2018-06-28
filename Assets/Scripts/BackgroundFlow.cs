b98defc31de80cba0ed3d77de97ded237ed8c089
jscstc007 <364080573@qq.com>
2018-06-28 15:12:14 +0800

    revert bg rotation

--- Assets/Scripts/BackgroundFlow.cs
+++ Assets/Scripts/BackgroundFlow.cs
@@ -22,39 +22,39 @@ public class BackgroundFlow : MonoBehaviour {
 
     private void InitUIPos ()
     {
-        BG_1.localPosition = Vector3.zero;
-        BG_2.localPosition = -BASE_ICON_VECTOR3;
+        BG_1.localPosition = BASE_ICON_VECTOR3;
+        BG_2.localPosition = Vector3.zero;
 
-        Star_1.localPosition = Vector3.zero;
-        Star_2.localPosition = -BASE_ICON_VECTOR3;
+        Star_1.localPosition = BASE_ICON_VECTOR3;
+        Star_2.localPosition = Vector3.zero;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        BG_1.localPosition += Time.deltaTime * new Vector3(0, BG_Speed, 0);
-        BG_2.localPosition += Time.deltaTime * new Vector3(0, BG_Speed, 0);
+        BG_1.localPosition -= Time.deltaTime * new Vector3(0, BG_Speed, 0);
+        BG_2.localPosition -= Time.deltaTime * new Vector3(0, BG_Speed, 0);
 
-        if (BG_1.localPosition.y >= ICON_DIS)
+        if (BG_1.localPosition.y <= -ICON_DIS)
         {
-            BG_1.localPosition -= 2 * BASE_ICON_VECTOR3;
+            BG_1.localPosition += 2 * BASE_ICON_VECTOR3;
         }
-        if (BG_2.localPosition.y >= ICON_DIS)
+        if (BG_2.localPosition.y <= -ICON_DIS)
         {
-            BG_2.localPosition -= 2 * BASE_ICON_VECTOR3;
+            BG_2.localPosition += 2 * BASE_ICON_VECTOR3;
         }
 
         // -----------------------------------------------------------------------------------
 
-        Star_1.localPosition += Time.deltaTime * new Vector3(0, Star_Speed, 0);
-        Star_2.localPosition += Time.deltaTime * new Vector3(0, Star_Speed, 0);
+        Star_1.localPosition -= Time.deltaTime * new Vector3(0, Star_Speed, 0);
+        Star_2.localPosition -= Time.deltaTime * new Vector3(0, Star_Speed, 0);
 
-        if (Star_1.localPosition.y >= ICON_DIS)
+        if (Star_1.localPosition.y <= -ICON_DIS)
         {
-            Star_1.localPosition -= 2 * BASE_ICON_VECTOR3;
+            Star_1.localPosition += 2 * BASE_ICON_VECTOR3;
         }
-        if (Star_2.localPosition.y >= ICON_DIS)
+        if (Star_2.localPosition.y <= -ICON_DIS)
         {
-            Star_2.localPosition -= 2 * BASE_ICON_VECTOR3;
+            Star_2.localPosition += 2 * BASE_ICON_VECTOR3;
         }
     }
 }
